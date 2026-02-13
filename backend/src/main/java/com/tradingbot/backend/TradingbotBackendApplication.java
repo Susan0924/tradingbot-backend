6e8725a969e23f368a634a458f278c0119ef7c74
unknown <MCA@MCA-CAT15.MCA.PSG>
2026-02-13 11:57:22 +0530

    Add Spring Boot base project

---
+++ backend/src/main/java/com/tradingbot/backend/TradingbotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tradingbot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingbotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingbotBackendApplication.class, args);
+	}
+
+}
